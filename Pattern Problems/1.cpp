1b447fa929407fe46a557927477a7c9ae12d2123
GOVIND KUMAR <70854788+g3vind@users.noreply.github.com>
2023-01-01 01:08:12 +0530

    Add files via upload

---
+++ Pattern Problems/1.cpp
@@ -0,0 +1,25 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // int n;
+    // cout << "Enter a no. ";
+    // cin >> n;
+
+    for (int row = 1; row <= 5; row++)
+    {
+
+        for (int col = 1; col <= 4; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
